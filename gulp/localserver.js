33b9698cc1e5f2e246fe9896ad88ba2d05a73975
1maximsafronov <1maximsafronov@gmail.com>
2020-05-04 20:50:39 +0700

    Добавляет стили для шапки страницы, промо блока, блока категорий товаров главной страницы

--- gulp/localserver.js
+++ gulp/localserver.js
@@ -2,17 +2,15 @@ const gulp = require(`gulp`);
 const server = require(`browser-sync`).create();
 const css = require(`./css`);
 const html = require(`./html`);
-const refresh = require(`./refresh`);
 const copy = require(`./copy`);
 
+function refresh(done) {
+  server.reload();
+  done();
+}
+
 module.exports = function localserver() {
-  server.init({
-    server: `build`,
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
+  server.init({server: `build`, notify: false, open: true, cors: true, ui: false});
 
   gulp.watch(`src/*.html`, gulp.series(html, refresh));
   gulp.watch(`src/img/icon-*.svg`, gulp.series(html, refresh));
